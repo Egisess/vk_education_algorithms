851e252e64fa739ce16b0cdd3fd600531935cbd6
egisess <Duckythunder214@gmail.com>
2025-02-25 02:14:01 +0300

    Подкорректировал тесты

--- sem1/two_sum/test_two_sum.py
+++ sem1/two_sum/test_two_sum.py
@@ -1,21 +1,24 @@
 import pytest
 from two_sum import solution_1
 
+test_nums = [1, 3, 5, 7, 13]
+
 
 @pytest.mark.parametrize("nums, target, expected", [
-    ([1, 3, 5, 7, 13], 4, [0, 1]),
-    ([1, 3, 5, 7, 13], 8, [0, 3]),
-    ([1, 3, 5, 7, 13], 12, [2, 3]),
-    ([1, 3, 5, 7, 13], 20, [3, 4]),
-    ([1, 3, 5, 7, 13], 6, [0, 2]),
-    ([1, 3, 5, 7, 13], 10, [1, 3]),
-    ([1, 3, 5, 7, 13], 18, [2, 4]),
-    ([1, 3, 5, 7, 13], 8, [0, 3]),
-    ([1, 3, 5, 7, 13], 16, [1, 4]),
-    ([1, 3, 5, 7, 13], 14, [0, 4]),
-    ([1, 3], 4, [0, 1]),
-    ([1, 3], 3, [-1, -1]),
-    ([1], 2, [-1, -1]),
+    (test_nums, 4, [[0, 1]]),
+    (test_nums, 8, [[0, 3], [1, 2]]),
+    (test_nums, 12, [[2, 3]]),
+    (test_nums, 20, [[3, 4]]),
+    (test_nums, 6, [[0, 2]]),
+    (test_nums, 10, [[1, 3]]),
+    (test_nums, 18, [[2, 4]]),
+    (test_nums, 8, [[0, 3], [1, 2]]),
+    (test_nums, 16, [[1, 4]]),
+    (test_nums, 14, [[0, 4]]),
+    ([-1, 1], 0, [[0, 1]]),
+    ([1, 3], 4, [[0, 1]]),
+    ([1, 3], 3, [[-1, -1]]),
+    ([1], 2, [[-1, -1]]),
 ])
 def test_solutions(nums, target, expected):
-    assert solution_1(nums, target) == expected
+    assert solution_1(nums, target) in expected
